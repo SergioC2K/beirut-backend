ff698347fbab699183fdb20b1eb434e24851a865
SergioC2K <agentcarvajal@gmail.com>
2024-08-14 14:31:58 +0200

    update white noise

--- beirutBackend/settings.py
+++ beirutBackend/settings.py
@@ -135,4 +135,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
